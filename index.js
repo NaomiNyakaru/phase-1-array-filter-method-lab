f177567f9d6afde9b909dea9d737317272698aec
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-07-04 20:35:12 +0300

    filter lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers,name){
+    return drivers.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+}
+console.log(findMatching());
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter(drivers => drivers.toLowerCase()[0] === name.toLowerCase()[0])
+}
+console.log(fuzzyMatch());
+
+function matchName(drivers,name){
+    return drivers.filter(drivers => drivers.name.toLowerCase() === name.toLowerCase());
+
+}
+console.log(matchName());
